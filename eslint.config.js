6ce8f91980ec39eac3601109a2d5d2259becf573
Vinicius Amorim <amorimm.viniciuss@gmail.com>
2026-06-24 18:04:40 -0300

    refactor: separate users routes in shared, infra, http

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      // Suas regras personalizadas aqui
+    },
+  },
+];
